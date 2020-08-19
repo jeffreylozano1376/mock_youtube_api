67e8c3f46a62e718e615c29ee908bf292f4d3560
jeffreylozano1376 <jeffreylozano1376@yahoo.com.ph>
2020-08-19 18:52:21 +0800

    fa

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
+# Flask Application Instance
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
